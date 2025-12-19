6b1769ef694917977776a05d0c3a2faea581fa2b
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-19 11:20:14 -0500

    init

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="./common.d.ts" />
+/// <reference path="./area.d.ts" />
+/// <reference path="./person.d.ts" />
+/// <reference path="./organization.d.ts" />
+/// <reference path="./event.d.ts" />
+/// <reference path="./motion.d.ts" />
+/// <reference path="./vote.d.ts" />
+/// <reference path="./speech.d.ts" />
+
+export = Popolo;
+export as namespace Popolo;
